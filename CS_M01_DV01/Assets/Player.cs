9143f26d0ffe03155e7e06c3b29cc1a22fc6698f
schwermc <schwermc@my.erau.edu>
2024-01-17 09:39:12 -0700

    The Start of M01 DV01

---
+++ CS_M01_DV01/Assets/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.5f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.5f;
+        }
+    }
+}
