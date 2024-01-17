bde44d0b26cb55c052e1e64d7421b8f192550291
schwermc <schwermc@my.erau.edu>
2024-01-17 09:41:21 -0700

    Random changed pt1
    This popped up after doing the "Start of M01 Dv01" commit to *main*.

--- CS_M01_DV01/Assets/Player.cs
+++ CS_M01_DV01/Assets/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using UnityEngine;
+
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
