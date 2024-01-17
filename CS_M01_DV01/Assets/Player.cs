d47771ef605a99d895896b1b99f4c6e7b387732a
schwermc <schwermc@my.erau.edu>
2024-01-17 13:46:12 -0700

    Movement Keys
    Add more keys to the player movement and up and down keys

--- CS_M01_DV01/Assets/Player.cs
+++ CS_M01_DV01/Assets/Player.cs
@@ -14,21 +14,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += Vector3.forward * 0.25f;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.back * 0.25f;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * 0.25f;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * 0.25f;
+        }
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.RightShift))
+        {
+            transform.position += Vector3.up * 0.25f;
+        }
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightControl))
+        {
+            transform.position += Vector3.down * 0.25f;
         }
     }
 }
