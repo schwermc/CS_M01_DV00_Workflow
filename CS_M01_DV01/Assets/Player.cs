97dc7c4cedc2cef089b88eed13a1da63ee0a0443
schwermc <schwermc@my.erau.edu>
2024-01-17 09:50:12 -0700

    Having WASD at .25f

--- CS_M01_DV01/Assets/Player.cs
+++ CS_M01_DV01/Assets/Player.cs
@@ -16,11 +16,19 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * 0.5f;
+            transform.position += Vector3.forward * 0.25f;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * 0.5f;
+            transform.position += Vector3.back * 0.25f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.25f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.25f;
         }
     }
 }
